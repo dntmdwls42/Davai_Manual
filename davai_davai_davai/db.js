b28010c2fb99ca256d9f410dd40388202b7949a8
woohojin <83582360+woohojin@users.noreply.github.com>
2025-01-04 21:17:15 +0900

    Add db.js + others for connect DB
    Vite dev Server port is set on 3000,
    Add some packages for connect to db and for sercurity,
    Remove mariadb because add mysql2 is better,

---
+++ davai_davai_davai/db.js
@@ -0,0 +1,33 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let db;
+
+export async function connectDB() {
+  if (!db) {
+    try {
+      db = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+      });
+      console.log("Successfully connected to the database.");
+    } catch (err) {
+      console.error("Failed to connect to Database : ", err.message);
+      throw err;
+    }
+  }
+
+  try {
+    await db.ping();
+  } catch (err) {
+    console.log("DB connection lost, reconnecting...", err);
+    db = null;
+    return connectDB();
+  }
+
+  return db;
+}
