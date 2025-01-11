0d3732b12baad188bba51d512a31d98c81c3075b
woohojin <83582360+woohojin@users.noreply.github.com>
2025-01-11 16:56:19 +0900

    Add DB_PORT env for change Mariadb Port

--- davai_davai_davai/db.js
+++ davai_davai_davai/db.js
@@ -13,6 +13,7 @@ export async function connectDB() {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
+        port: process.env.DB_PORT,
       });
       console.log("Successfully connected to the database.");
     } catch (err) {
