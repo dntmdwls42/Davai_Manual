8447fb32fd72cff550ef7bd1f1d564be5ed8ab15
woohojin <83582360+woohojin@users.noreply.github.com>
2025-01-22 20:05:55 +0900

    Apply background image in all pages

--- davai_davai_davai/src/jsx/main.jsx
+++ davai_davai_davai/src/jsx/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Minigame from "./Minigame";
 import Weapon from "./Weapon";
+import Background from "./Background";
 import "../css/index.css";
 import "../css/background.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
+    <Background />
     <Routes>
       <Route path="/" element={<App />}></Route>
       <Route path="/Minigame" element={<Minigame />}></Route>
