4d5280ee8d181a9f9eeedf5f12e25faa35295755
woohojin <83582360+woohojin@users.noreply.github.com>
2025-01-27 17:48:50 +0900

    Background image changed to webp

--- davai_davai_davai/src/jsx/Background.jsx
+++ davai_davai_davai/src/jsx/Background.jsx
@@ -4,7 +4,7 @@ function Background() {
   return (
     <>
       <div className="background-container">
-        <img className="background-image" src="/image/Background.png"></img>
+        <img className="background-image" src="/image/Background.webp"></img>
       </div>
     </>
   );
