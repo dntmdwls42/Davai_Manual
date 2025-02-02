ab1ad5218c0810d9736bfb1bc758f404c93cdee8
woohojin <83582360+woohojin@users.noreply.github.com>
2025-02-02 17:00:05 +0900

    Update variable names

--- davai_davai_davai/src/jsx/Layout.jsx
+++ davai_davai_davai/src/jsx/Layout.jsx
@@ -22,11 +22,11 @@ function Layout() {
             }}
           >
             {/*style의 margin은 아이콘이 가운데에 맞도록 정렬 한 것*/}
-            <div className="sidebar_icon center" onClick={handleSidebarToggle}>
+            <div className="sidebar__icon center" onClick={handleSidebarToggle}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 320 512"
-                className={`${isSidebarVisible ? "is-visible" : ""}`}
+                className={`${isSidebarVisible ? "is-hidden" : ""}`}
                 style={{ marginRight: "3px" }}
               >
                 <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
@@ -34,13 +34,13 @@ function Layout() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 320 512"
-                className={`${isSidebarVisible ? "" : "is-visible"}`}
+                className={`${isSidebarVisible ? "" : "is-hidden"}`}
                 style={{ marginLeft: "3px" }}
               >
                 <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
               </svg>
             </div>
-            <div className="sidebar_content max-size">
+            <div className="sidebar__content max-size">
               <ul>
                 <li>
                   <Link to={`/animationTest`}>A</Link>
