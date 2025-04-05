37042a0252274d07f5ce8c2e27b1d842f59927ef
WSJ <dntmdwls42@wku.ac.kr>
2025-04-05 17:43:35 +0900

    Refactor main.jsx to routing minigame.jsx

--- davai_davai_davai/src/jsx/Minigame.jsx
+++ davai_davai_davai/src/jsx/Minigame.jsx
@@ -1,9 +1,16 @@
-import Menu from "./Menu.jsx";
+import { Routes, Route } from "react-router-dom";
+import MinigameMenu from "./MinigameMenu.jsx";
+import Weapon from "./Weapon.jsx";
+import WeaponAndCaliber from "./WeaponAndCaliber.jsx";
 
 function Minigame() {
   return (
     <>
-      <Menu />
+      <Routes>
+        <Route path="/" element={<MinigameMenu />}></Route>
+        <Route path="Weapon" element={<Weapon />}></Route>
+        <Route path="WeaponAndCaliber" element={<WeaponAndCaliber />}></Route>
+      </Routes>
     </>
   );
 }
--- davai_davai_davai/src/jsx/main.jsx
+++ davai_davai_davai/src/jsx/main.jsx
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 import Minigame from "./Minigame";
-import Weapon from "./Weapon";
 import Background from "./Background";
 import AnimationTest from "./AnimationTest";
-import WeaponAndCaliber from "./WeaponAndCaliber";
 import Layout from "./Layout";
 import Header from "./Header";
 import "../css/index.css";
@@ -20,12 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Layout />
     <Routes>
       <Route path="/" element={<App />}></Route>
-      <Route path="/Minigame" element={<Minigame />}></Route>
-      <Route path="/Minigame/Weapon" element={<Weapon />}></Route>
-      <Route
-        path="/Minigame/WeaponAndCaliber"
-        element={<WeaponAndCaliber />}
-      ></Route>
+      <Route path="/Minigame/*" element={<Minigame />}></Route>
       <Route path="/AnimationTest" element={<AnimationTest />}></Route>
     </Routes>
   </Router>,
