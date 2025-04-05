68defbf481c9bfb3883c9cd37eff667069c4ce5e
WSJ <dntmdwls42@wku.ac.kr>
2025-04-05 14:48:28 +0900

    Update Minigame menu font and styles

--- davai_davai_davai/src/jsx/Menu.jsx
+++ davai_davai_davai/src/jsx/Menu.jsx
@@ -11,7 +11,7 @@ function Menu() {
 
   return (
     <>
-      <div className="page-container">
+      <div id="minigame-container" className="page-container">
         <div className="minigame-menu">
           <h1 className="minigame-menu__title">총기 퀴즈 선택</h1>
           {/* 아래에 삽입된 이미지는 임시임. */}
@@ -20,48 +20,32 @@ function Menu() {
             src="/image/USEC-gun-aiming.webp"
             alt="USEC-gun-aiming"
           />
-          <div className="minigame-menu__quiz-selector-container">
-            <div className="menu-container">
-              <div>
-                <label className="mimigame-menu__quiz__selector">
-                  총기 문제 갯수:
-                  <select value={quizNumber} onChange={handleNumber}>
-                    <option value={5}>5개</option>
-                    <option value={10}>10개</option>
-                    <option value={15}>15개</option>
-                  </select>
-                </label>
-              </div>
-
-              {/* 선택한 문제 갯수로 Weapon 페이지로 이동 */}
-              <Link
-                to={`/Minigame/Weapon?number=${quizNumber}`}
-                style={{ margin: "10px" }}
-              >
-                문제 시작
-              </Link>
-            </div>
-            <div className="menu-container">
-              <div>
-                <label className="mimigame-menu__quiz__selector">
-                  총기 구경 문제 갯수:
-                  <select value={quizNumber} onChange={handleNumber}>
-                    <option value={5}>5개</option>
-                    <option value={10}>10개</option>
-                    <option value={15}>15개</option>
-                  </select>
-                </label>
-              </div>
-
-              {/* 선택한 문제 갯수로 Weapon 페이지로 이동 */}
-              <Link
-                to={`/Minigame/WeaponAndCaliber?number=${quizNumber}`}
-                style={{ margin: "10px" }}
-              >
-                <div>문제 시작</div>
-              </Link>
-            </div>
+          <div className="minigame-menu__quiz-selector">
+            <p className="minigame-menu__quiz-selector__text">
+              문제 유형과 갯수를 선택하세요
+            </p>
+            <select className="minigame-menu__quiz-type-selector">
+              <option value="1">총기 이름 맞추기</option>
+              <option value="2">총기 구경 맞추기</option>
+            </select>
+            <select
+              className="minigame-menu__quiz-number-selector"
+              value={quizNumber}
+              onChange={handleNumber}
+            >
+              <option value={5}>5개</option>
+              <option value={10}>10개</option>
+              <option value={15}>15개</option>
+            </select>
           </div>
+          <Link
+            to={`/Minigame/Weapon?number=${quizNumber}`}
+            style={{ margin: "10px" }}
+          >
+            <button className="minigame-menu__quiz-selector__button">
+              <p>문제 시작</p>
+            </button>
+          </Link>
         </div>
       </div>
     </>
